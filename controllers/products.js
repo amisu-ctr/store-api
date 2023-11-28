2c0061b158c46bd8a038dd2a62b1d34b80bbaac9
amisu <hamisurahman@gmail.com>
2023-11-28 18:55:48 +0000

    populate database

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,16 @@
-const getAllProductsStatic = async (req, res) =>  {
-    res.status(200).json({msg:'products testing route'})
+const Product = require('../models/product')
+
+
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({featured: true})
+    res.status(200).json({products})
+}
+const getAllProducts = async (req, res) => {
+    throw new Error('testing async errors')
+    res.status(200).json({msg: 'products route'})
 }
 
-const getAllProducts = async (req, res) =>  {
-    res.status(200).json({msg:'products route'})
-}
-
-
-module.export = {
-    getAllProducts ,
-    getAllProductsStatic,
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic
 }
--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,7 @@ const router = express.Router()
 
 const {getAllProductsStatic, getAllProducts} = require('../controllers/products')
 
-router.route('/').get( (req, res) =>  {
-    throw new Error('testing async errors')
-    res.status(200).json({msg:'products testing route'})
-})
-router.route('/static').get((req, res) =>  {
-    throw new Error('testing async errors')
-    res.status(200).json({msg:'products route'})
-})
+router.route('/').get( getAllProducts)
+router.route('/static').get(getAllProductsStatic)
 
 module.exports = router
